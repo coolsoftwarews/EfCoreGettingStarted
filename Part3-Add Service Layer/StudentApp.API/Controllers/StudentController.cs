55e72ac9210a3ffba3eceb6a81ae1c273bdf31c2
Gabriel Le Roux <Gabriel.LeRoux@westerncape.gov.za>
2022-03-06 14:39:23 +0200

    add code first migration

---
+++ Part3-Add Service Layer/StudentApp.API/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Core.Domain;
+using StudentApp.Data;
+
+namespace StudentApp.API.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private StudentContext _context;
+
+        public StudentController()
+        {
+            this._context = new StudentContext();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+
+         var students = await _context.Students
+                                    .ToListAsync();
+
+            return Ok(students);
+        }
+
+        [HttpGet("{id}", Name ="GetStudentById")]
+        public async Task<IActionResult> Get(int id)
+        {
+
+            return Ok(await this._context.Students.FirstOrDefaultAsync(x => x.Id == id));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Student student)
+        {
+                this._context.Add(student); 
+
+                await this._context.SaveChangesAsync();
+
+                return Ok(student);
+
+        }
+
+    }
+}
---
+++ Part3-Add Service Layer/StudentApp.Data/StudentContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApp.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Data
+{
+    public class StudentContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog = StudentDB");
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
