08a60bcca0689a12ec0e0245e6e494ec301c03a4
Gabriel Le Roux <Gabriel.LeRoux@westerncape.gov.za>
2022-03-06 19:01:31 +0200

    init

---
+++ Part4-AutoMapper/StudentApp.API/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Core.Domain;
+using StudentApp.Data;
+using StudentApp.Services;
+using StudentApp.Services.DTOs;
+
+namespace StudentApp.API.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IStudentService _studentService;
+
+
+        public StudentController(
+                        IStudentService studentService
+            )
+        {
+            this._studentService = studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await this._studentService.GetAllAsync());
+        }
+
+        [HttpGet("{id}", Name = "GetStudentById")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await this._studentService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] dtoStudent dto)
+        {
+            await this._studentService.CreateAsync(dto);
+
+            return Ok(dto);
+        }
+
+    }
+}
